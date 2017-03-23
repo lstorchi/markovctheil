ec721321876ed950c52adefb17f5194ae1fae182
Loriano Storchi <loriano@storchi.org>
2017-03-23 15:04:47 +0100

    entrypi test

--- testentropy.py
+++ testentropy.py
@@ -10,16 +10,39 @@ import os
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-fp = open("entropy_100.txt")
-
+filename = "entropy_100.txt"
 a = 0.1
-#0.05 
-#0.15
 
+if len(sys.argv) != 3:
+    print "usage: ", sys.argv[0], \
+            " filename avalue" 
+    exit(1)
+else:
+    filename = sys.argv[1] 
+    a = float(sys.argv[2])
+
+fp = open(filename)
+
+m = []
+mmax = []
+mmin = []
+cmpval = []
+
+idx = 0
 for l in fp:
-    print 1.0 - (float(l.split(" ")[2]) / a**2)
+    sval = l.split(" ")
+    idx += 1
 
-    print float(l.split(" ")[1]) + a
-    print float(l.split(" ")[1]) - a
+    if len(sval) == 4:
+      cmpval.append(1.0 - (float(sval[2]) / a**2))
+      mmax.append(float(sval[1]) + a)
+      mmin.append(float(sval[1]) - a)
+      m.append(float(sval[1]))
+    else:
+      print "error at line: ", idx 
+
+idx = 0
+for idx in range(len(m)):
+    print idx+1, " ", mmin[idx], " ", m[idx], " ", mmax[idx] , " ", cmpval[idx]
 
 fp.close()
