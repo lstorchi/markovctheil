2f330ed994975b8dd58f35236da9388381b160dc
Loriano Storchi <loriano@storchi.org>
2017-04-20 11:46:42 +0200

    backup

--- options.py
+++ options.py
@@ -7,11 +7,15 @@ class optiondialog(QtGui.QDialog):
 
         self.okbutton = QtGui.QPushButton('Ok');
         self.okbutton.setFont(QtGui.QFont("Times", 10, QtGui.QFont.Bold));
-
         self.connect(self.okbutton, QtCore.SIGNAL("clicked()"), self.closedialog)
 
-        self.verticalLayout = QtGui.QVBoxLayout(self)
-        self.verticalLayout.addWidget(self.okbutton)
+        self.inftcb = QtGui.QCheckBox("Simulate infinite time", self)
+        self.inftcb.setChecked(False)
+
+        self.grid = QtGui.QGridLayout(self)
+
+        self.grid.addWidget(self.inftcb)
+        self.grid.addWidget(self.okbutton)
 
     def closedialog(self):
         self.close()
