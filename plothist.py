79236436ba18bce2624fef91b8b04eef5b54805c
Loriano Storchi <loriano@storchi.org>
2017-04-27 13:07:39 +0200

    backup

--- plothist.py
+++ plothist.py
@@ -1,6 +1,10 @@
 from PyQt4 import QtCore, QtGui
 import sys
 
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+
 sys.path.append("./module")
 import basicutils
 
@@ -12,5 +16,17 @@ class plohitwin (QtGui.QMainWindow):
         self.resize(640, 480) 
         self.setWindowTitle('Plots')
 
-    def closedialog(self):
-        self.close()
+        figure = plt.figure()
+        canvas = FigureCanvas(figure)
+        toolbar = NavigationToolbar(canvas, self)
+
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(toolbar)
+        layout.addWidget(canvas)
+
+        maindialog = QtGui.QWidget()
+        maindialog.setLayout(layout)
+
+        self.setCentralWidget(maindialog)
+
+
