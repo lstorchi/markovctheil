4ff3627ae3c0fd32d6c19e3716311d18b6746d2e
Loriano Storchi <loriano@storchi.org>
2017-06-20 16:39:03 +0200

    minor to dump matlab style file

--- test/preparetbl.py
+++ test/preparetbl.py
@@ -3,6 +3,7 @@ import numpy
 import pandas
 import datetime
 import calendar
+import scipy.io
 
 sys.path.append("../module")
 import basicutils
@@ -50,13 +51,28 @@ for i in range(1,len(cn)):
         j = j + 1
     c = c + 1
 
+tot = 0
+for i in range(0,len(dates)):
+    y0 = int(dates[i][0])
+    m0 = int(dates[i][1])
+    ld = calendar.monthrange(y0,m0)
+
+    tot = tot + ld[1]
+
+nmat = numpy.zeros((count, tot))
+
 for k in range(mat.shape[0]):
-    sys.stdout.write("%s ,"%(cnames[k]))
+    #sys.stdout.write("%s ,"%(cnames[k]))
+    aidx = 0
     for i in range(0,len(dates)):
         y0 = int(dates[i][0])
         m0 = int(dates[i][1])
         ld = calendar.monthrange(y0,m0)
 
         for j in range(ld[1]):
-            sys.stdout.write("%f , "%(mat[k,i]))
-    sys.stdout.write(" %s \n"%(cnames[k]))
+            nmat[k, aidx] = mat[k,i]
+            aidx = aidx + 1
+            #sys.stdout.write("%f , "%(mat[k,i]))
+    #sys.stdout.write(" %s \n"%(cnames[k]))
+
+scipy.io.savemat('data.mat', mdict={'nmat': nmat})
