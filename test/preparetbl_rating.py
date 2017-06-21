59034eb98e13174f7f9ec59d94eda9c9b1a0a452
Loriano Storchi <loriano@storchi.org>
2017-06-21 17:28:18 +0200

    added 1998 filter

--- test/preparetbl_rating.py
+++ test/preparetbl_rating.py
@@ -15,27 +15,35 @@ def basic_repter (sep):
 
     val = [] 
 
+    rat2num = {"AAA": 1, "Aaa":1, "AA": 2, "Aa":2, "AA-": 2, "AA+": 2, \
+            "Aa1":2, "Aa2":2, "Aa3":2, "A+":3, "A":3,  "A-":3, \
+            "A1":3, "A2":3, "A3":3, "BBB+":4, "BBB":4, "BBB-":4, \
+            "Baa1":4, "Baa2":4, "Baa3":4, "BB+":5, "BB":5, "BB-":5, \
+            "Ba1":5, "Ba2":5, "Ba3":5, "B+":6, "B":6, "B-":6, "B1":6, \
+            "B2":6, "B3":6, "CCC+":7, "CCC":7, "CCC-":7, "CC":7, "C":7, \
+            "Caa1":7, "Caa2":7, "Caa3":7, "Ca":7, "SD":8, "D":8, "RD":8}
+
     nametonum = dict((v,k) for k,v in enumerate(calendar.month_abbr))
 
-    for i in range(1, len(sep)):
-        date1 = sep[i-1][1]
+    m2 = 5 
+    d2 = 31
+    y2 = 2017
+    for i in range(0, len(sep)):
+        date1 = sep[i][1]
         vdate1 = date1.split()
         m1 = nametonum[vdate1[0]]
         d1 = int(vdate1[1])
         y1 = int(vdate1[2])
 
-        date2 = sep[i][1]
-        vdate2 = date2.split()
-        m2 = nametonum[vdate2[0]]
-        d2 = int(vdate2[1])
-        y2 = int(vdate2[2])
-        
-        d1 = datetime.date(y1, m1, d1)
-        d2 = datetime.date(y2, m2, d2)
-        delta = d1 - d2 
-        for j in range(delta.days-1): # -1 is really neeeded
-            val.append(sep[i-1][0])
+        da1 = datetime.date(y1, m1, d1)
+        da2 = datetime.date(y2, m2, d2)
+        delta = da2 - da1
+        for j in range(delta.days): # -1 is really neeeded
+            val.append(rat2num[sep[i-1][0]])
 
+        m2 = m1
+        d2 = d1
+        y2 = y1
 
     return val
 
@@ -47,26 +55,57 @@ def dump_file (count, values):
      moody = []
      fitch = [] 
      
+     tododsep = True
+     tododmoody = True
+     tododfitch = True
+
+     nametonum = dict((v,k) for k,v in enumerate(calendar.month_abbr))
+
      for v in values:
-         if len(v) == 3:
-           if v[0] == "S&P":
-               sep.append([v[1], v[2]])
-           elif v[0] == "Moody\'s":
-               moody.append([v[1], v[2]])
-           elif v[0] == "Fitch":
-               fitch.append([v[1], v[2]])
-         elif len(v) == 4:
-           if v[0] == "S&P":
-               sep.append([v[1], v[3]])
-           elif v[0] == "Moody\'s":
-               moody.append([v[1], v[3]])
-           elif v[0] == "Fitch":
-               fitch.append([v[1], v[3]])
+         if (v[0] == "S&P" ) or (v[0] == "Moody\'s") or \
+                 (v[0] == "Fitch"):
+            date = ""
+            
+            if len(v) == 3:
+                date = v[2]
+            elif len(v) == 4:
+                date = v[3]
+            
+            vdate = date.split()
+            m = nametonum[vdate[0]]
+            d = int(vdate[1])
+            y = int(vdate[2])
+            
+            if (y < 1998):
+                date = "Jan 01 1998"
+            
+            if v[0] == "S&P":
+                if tododsep:
+                  sep.append([v[1], date])
+            elif v[0] == "Moody\'s":
+                if tododmoody:
+                  moody.append([v[1], date])
+            elif v[0] == "Fitch":
+                if tododfitch:
+                  fitch.append([v[1], date])
+            
+            if (y < 1998):
+                if v[0] == "S&P":
+                    tododsep = False
+                elif v[0] == "Moody\'s":
+                    tododmoody = False
+                elif v[0] == "Fitch":
+                    tododfitch = False
      
+     #print c 
+     #print "SeP"
      vsep = basic_repter (sep)
+     #print "Mod"
      vmoody = basic_repter (moody)
+     #print "fitch"
      vfitch = basic_repter (fitch)
-     
+     #print "" 
+
      outfilename = count+"_data.mat"
      
      if os.path.exists(outfilename):
@@ -98,6 +137,5 @@ for v in cn:
 
 for c in countries:
     vals = df[c].values
-    #print vals
     dump_file (c, vals)
 
