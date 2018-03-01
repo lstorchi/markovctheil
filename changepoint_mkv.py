397a8b6f868cbaea66cb8e3adc6b891165d7fbe9
Loriano Storchi <loriano@storchi.org>
2018-03-01 09:30:59 +0100

    added changemodule

--- changepoint_mkv.py
+++ changepoint_mkv.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 sys.path.append("./module")
 import basicutils
+import changemod
 
 filename = ""
 rownum = 0
@@ -42,95 +43,12 @@ rating=numpy.max(rm)
 time=rm.shape[1]
 c_p=time/2
 
-if (rating <= 0) or (rating > 8):
-    errmsg.append("rating " + rating + " is not a valid value")
+errmsg = ""
+L, L1, L2 = changemod.compute_ls(rm, c_p, rownum, errmsg)
+
+if (L == None):
+    print errmsg
     exit(1)
-
-num = numpy.zeros((rating,rating), dtype='int64')
-den = numpy.zeros(rating, dtype='int64')
-pr = numpy.zeros((rating,rating), dtype='float64')
-
-L = 0.0
-
-for i in range(rating):
-    for j in range(rating):
-        for t in range(time-1):
-            if (rm[rownum, t] == (i+1)) and (rm[rownum, t+1] == (j+1)):
-                num[i, j] = num[i, j] + 1
-                
-    den[i] = sum(num[i])
-
-    if (den[i] > 0.0):
-        for j in range(rating):
-            val = numpy.float64(num[i,j])/numpy.float64(den[i])
-            if (val > 0.0):
-                L += num[i,j]*math.log(val) 
-
-for i in range(rating):
-    for j in range(rating):
-        if den[i] != 0:
-           pr[i, j] = float(num[i, j])/float(den[i])
-        else: 
-           pr[i, j] = 0
-           pr[i, i] = 1
-   
-num1 = numpy.zeros((rating,rating), dtype='int64')
-den1 = numpy.zeros(rating, dtype='int64')
-pr1 = numpy.zeros((rating,rating),dtype='float64')
-
-L1 = 0.0 
-
-for i in range(rating):
-     for j in range(rating):
-          for t in range(c_p-1):
-             if (rm[rownum, t] == (i+1)) and (rm[rownum, t+1] == (j+1)):
-                 num1[i, j] = num1[i, j] + 1
-        
-
-     den1[i] = sum(num1[i])
-
-     if (den1[i] > 0.0):
-        for j in range(rating):
-           val = numpy.float64(num1[i,j])/numpy.float64(den1[i])
-           if (val > 0.0):
-              L1 += num1[i,j]*math.log(val) 
-
-for i in range(rating):
-    for j in range(rating):
-        if den1[i] != 0:
-           pr1[i, j] = float(num1[i, j])/float(den1[i])
-        else: 
-           pr1[i, j] = 0        
-           pr1[i,i] = 1  
-
-
-num2 = numpy.zeros((rating,rating), dtype='int64')
-den2 = numpy.zeros(rating, dtype='int64')
-pr2 = numpy.zeros((rating,rating),dtype='float64')
- 
-L2 = 0.0 
-
-for i in range(rating):
-     for j in range(rating):
-         for t in range(c_p,time-1) :
-             if (rm[rownum, t] == (i+1)) and (rm[rownum, t+1] == (j+1)):
-                 num2[i, j] = num2[i, j] + 1
-         
-     den2[i] = sum(num2[i])
-
-     if (den2[i] > 0.0):
-         for j in range(rating):
-            val = numpy.float64(num2[i,j])/numpy.float64(den2[i])
-            if (val > 0.0):
-                L2 += num2[i,j]*math.log(val) 
- 
-for i in range(rating):
-    for j in range(rating):
-        if den2[i] != 0.0:
-           pr2[i, j] = float(num2[i, j])/float(den2[i])
-        else: 
-           pr2[i, j] = 0
-           pr2[i,i] = 1
 
 #print L, L1, L2
 lamda = -2*((L1+L2)-L)
