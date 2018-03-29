123b9c4b096e59d6850ae691b8f984dab071716b
Loriano Storchi <loriano@storchi.org>
2018-03-29 10:25:10 +0200

    addede utility script

---
+++ runmult_1.sh
@@ -0,0 +1,11 @@
+export start=1
+
+for num in $(seq 1 32)
+do
+  echo $num
+  export end=$(($start + 50))
+  echo $start, $end
+  python changepoint_unkn_3.py ./files/sep.mat $start $end 1095 > changepoint_unkn_"$num".out 2> changepoint_unkn_"$num".err
+  start=$(($end + 1))
+done
+
