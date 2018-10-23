4bac0ca0bf6301c68b34521607a50fdfaebe9950
Loriano Storchi <loriano@storchi.org>
2018-10-23 12:23:17 +0200

    backup

--- compute_entropy.py
+++ compute_entropy.py
@@ -24,30 +24,45 @@ r = msd[namems] #traiettorie spread simulate
 countries = r.shape[0]
 time = r.shape[1]
 run = r.shape[2]
-print countries, time, run
+print "countries: ", countries, "time: ", time, "run: ", run
 
 R = numpy.sum(r, axis=0)
+print "R.shape: ", R.shape
+
 Rm = numpy.zeros((time), dtype = 'float64')
-pm = numpy.zeros((countries,time), dtype = 'float64')
 entropy = numpy.zeros((time), dtype = 'float64')
 std = numpy.zeros((time), dtype = 'float64')
 T = numpy.zeros((time, run), dtype = 'float64')
 
-for i in range(run):
-    for j in range(countries):
-        for t in range(time):
-            p = r[j,t,i] / R[t,i]
-            #pm[j,t] = numpy.mean(p[j,t])
-            if p != 0:
-                T[t,i] += p*math.log(float(countries) * p)
 
+meanp = numpy.zeros((countries,time), dtype = 'float64')
+
+
+for j in range(countries):
+
+    for t in range(time):
+
+        pm = numpy.float64(0.0)
+
+        for i in range(run):
+            p = r[j,t,i] / R[t,i]
+
+            pm += p
+
+            if p != 0.0:
+                T[t,i] += p*math.log(float(countries) * p)
+        
+        pm = pm / numpy.float64(run) 
+
+        meanp[j, t] = pm
 
 for t in range(time):
-    entropy[t] = numpy.mean(T[t])
-    std[t] = numpy.std(T[t])
+    entropy[t] = numpy.mean(T[t, :])
+    std[t] = numpy.std(T[t, :])
 
-basicutils.mat_to_file(entropy, 'edt.txt')
-basicutils.mat_to_file(std, 'std.txt')
+basicutils.vct_to_file(entropy, 'edt.txt')
+basicutils.vct_to_file(std, 'std.txt')
+basicutils.mat_to_file(meanp, 'meanp.txt')
 
 plt.plot(entropy)
 plt.show()
